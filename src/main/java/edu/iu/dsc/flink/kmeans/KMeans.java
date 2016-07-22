1d4f29a022a37c8545bfef42ecdcbb9b7eeb2d19
supunkamburugamuve <supun06@gmail.com>
2016-07-22 13:27:45 -0400

    adding all the times

--- src/main/java/edu/iu/dsc/flink/kmeans/KMeans.java
+++ src/main/java/edu/iu/dsc/flink/kmeans/KMeans.java
@@ -151,19 +151,15 @@ public class KMeans {
         // feed new centroids back into next iteration
         DataSet<Centroid> finalCentroids = loop.closeWith(newCentroids);
 
-        DataSet<Tuple2<Integer, Point>> clusteredPoints = points
-                // assign points to final clusters
-                .map(new SelectNearestCenter()).withBroadcastSet(finalCentroids, "centroids");
-
         // emit result
         if (params.has("output")) {
-            clusteredPoints.writeAsCsv(params.get("output"), "\n", " ");
+            finalCentroids.writeAsCsv(params.get("output"), "\n", " ");
 
             // since file sinks are lazy, we trigger the execution explicitly
             env.execute("KMeans Example");
         } else {
             System.out.println("Printing result to stdout. Use --output to specify output path.");
-            clusteredPoints.print();
+            finalCentroids.print();
         }
     }
 
