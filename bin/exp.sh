3cea3bcf4ee6ed51398bd236a5378567dd7af861
supunkamburugamuva <supun06@gmail.com>
2016-09-25 16:34:50 -0400

    adding point input format

--- bin/exp.sh
+++ bin/exp.sh
@@ -5,8 +5,8 @@ OUT_DIR=$2
 
 points=( 1000 5000 10000 20000 )
 
-mkdir -p $OUT_DIR
 for i in "${points[@]}"
 do
-	./damds.sh $INPUT_DIR/$i/dist.bin $INPUT_DIR/$i/weight.bin $INPUT_DIR/$i/init_points $i $OUT_DIR
+    mkdir -p $OUT_DIR/$i
+	./damds.sh $INPUT_DIR/$i/dist.bin $INPUT_DIR/$i/weight.bin $INPUT_DIR/$i/init_points $i $OUT_DIR/$i
 done
