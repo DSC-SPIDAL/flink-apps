255235f15a60a8deaf43a1d8db64838c14c9af6b
supunkamburugamuve <supun06@gmail.com>
2016-09-25 14:34:02 -0400

    ad

--- bin/datagen.sh
+++ bin/datagen.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-java -cp ../target/flink-apps-0.1.jar edu.iu.dsc.flink.mm.MatrixFileGenerator -n $1 -m $2 -f $3 -t $4
+java -cp ../target/flink-apps-0.1-jar-with-dependencies.jar edu.iu.dsc.flink.mm.MatrixFileGenerator -n $1 -m $2 -f $3 -t $4
