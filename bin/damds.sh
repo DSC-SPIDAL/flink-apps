ed5c43f6b3bd585476a5a0af0720172ebd8c8ae7
supunkamburugamuva <supun06@gmail.com>
2016-09-25 16:31:53 -0400

    adding point input format

--- bin/damds.sh
+++ bin/damds.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-./flink run flink-apps-0.1.jar -dFile $1 -wFile $2 -pFile $3 -points $4 -outFolder $5
+flink run flink-apps-0.1.jar -dFile $1 -wFile $2 -pFile $3 -points $4 -outFolder $5
