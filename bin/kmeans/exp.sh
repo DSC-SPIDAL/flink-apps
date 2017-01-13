70ed5191d59aa06b9b0c54b67159c79fe3b0f67f
Supun Kamburugamuve <supun06@gmail.com>
2017-01-12 21:26:48 -0500

    adding : :

--- bin/kmeans/exp.sh
+++ bin/kmeans/exp.sh
@@ -6,11 +6,14 @@ tasks_per_node=20
 parallel=$(($2 * $tasks_per_node))
 echo "Parallel" $parallel
 points=hdfs://j-001:9001/kmeans/small/points/$p
+#points=hdfs://j-001:9001/kmeans/points/$p
 echo "POints: " $points
 centroids=hdfs://j-001:9001/kmeans/small/centers/$centers
+#centroids=hdfs://j-001:9001/kmeans/centers/$centers
 echo "Centroids: " $centroids
 #output=hdfs://j-001:9001/kmeans/out
 output=/N/u/skamburu/data/kmeans/output/small/${p}_${centers}_${parallel}
+#output=/N/u/skamburu/data/kmeans/output/${p}_${centers}_${parallel}
 echo "Output: " $output
 iterations=100
 k=$centers
